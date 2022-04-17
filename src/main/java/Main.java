5b3e6f828d9331912c4fd618be05629b1ffe2da1
Russell-Hill <88007515+Russell-Hill@users.noreply.github.com>
2022-04-16 20:02:40 -0400

    Capitalize

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ package main.java;
 
 public class Main {
         public static void main(String[] args){
-            System.out.println("hiya folks");
+            System.out.println("Hiya folks");
     }
 }
